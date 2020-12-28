825d40ba3bf9e0005911d28dbb9b532af499289e
makanai5610 <takumi.k.5610@gmail.com>
2020-12-28 15:29:09 +0900

    add go-git

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module ggc
 
 go 1.15
+
+require github.com/go-git/go-git/v5 v5.2.1-0.20201201095253-51cbc24bbecf
--- main.go
+++ main.go
@@ -1,4 +1,42 @@
 package main
 
+import (
+	"fmt"
+	"os"
+
+	"github.com/go-git/go-git/v5"
+)
+
 func main() {
+	repository, err := git.PlainOpen(".")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("repositoy:\n%+v\n", repository)
+
+	worktree, err := repository.Worktree()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("worktree:\n%+v\n", worktree)
+
+	if err := worktree.Checkout(&git.CheckoutOptions{Branch: "refs/heads/hoge", Create: false}); err != nil {
+		panic(err)
+	}
+
+	blobHash, err := worktree.Add(".")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("blob hash is %s\n", blobHash)
+
+	commitHash, err := worktree.Commit("message", &git.CommitOptions{All: true})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("commit hash is %s\n", commitHash)
+
+	if err := repository.Push(&git.PushOptions{Progress: os.Stdout}); err != nil {
+		panic(err)
+	}
 }
